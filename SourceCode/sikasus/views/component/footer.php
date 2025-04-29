7cd75c869d85e2433ace0ec04862783cf5946332
al-kawsar <tlod9906@gmail.com>
2025-04-29 14:49:18 +0800

    docs: update documentation

--- SourceCode/sikasus/views/component/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"></script>
-<script src=<?= "/admin-template-sb/js/scripts.js" ?>></script>
-</body>
-
-</html>
